c3adba0074a2f388641637b8f7e3a08d7c002a13
Malivuca <miguelvargasdemarque@gmail.com>
2022-11-15 20:01:37 -0300

    Criado esqueleto das requisicoes GET

---
+++ src/backend/requisitions.js
@@ -0,0 +1,78 @@
+import axios from 'axios'
+
+const PORT = 4000
+
+export const getAllPF = axios
+  .get(`localhost:${PORT}/URI`)
+  .then(function (response) {
+    // handle success
+    console.log(response)
+    return response
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error)
+  })
+
+export const getAuxRecPF = axios
+  .get(`localhost:${PORT}/URI`)
+  .then(function (response) {
+    // handle success
+    console.log(response)
+    return response
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error)
+  })
+
+export const getAuxBusPF = axios
+  .get(`localhost:${PORT}/URI`)
+  .then(function (response) {
+    // handle success
+    console.log(response)
+    return response
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error)
+  })
+
+// -- Relação de todos os governos com suas ofertas de vagas de emprego
+export const getEmpOferecidos = axios
+  .get(`localhost:${PORT}/URI`)
+  .then(function (response) {
+    // handle success
+    console.log(response)
+    return response
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error)
+  })
+
+// -- Relação de todos os governos com a quantidade de auxilios oferecidos
+export const getQuantAuxOferecidos = axios
+  .get(`localhost:${PORT}/URI`)
+  .then(function (response) {
+    // handle success
+    console.log(response)
+    return response
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error)
+  })
+
+// -- Relação de todos os governos com seus auxílios oferecidos
+export const getAuxOferecidos = axios
+  .get(`localhost:${PORT}/URI`)
+  .then(function (response) {
+    // handle success
+    console.log(response)
+    return response
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error)
+  })
